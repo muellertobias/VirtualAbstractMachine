969bc271d99bab863d1991c4a1527516b7b4b695
Tobias Müller <muellertobias789@gmail.com>
2017-10-24 09:47:34 +0200

    Began to implement a stack loader

---
+++ VirtualAbstractMachine/Utilities/StackLoader/StringStackLoader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualAbstractMachine.VAM;
+
+namespace VirtualAbstractMachine.Utilities.StackLoader
+{
+    public class StringStackLoader : AbstractStackLoader
+    {
+        public override Stack Load(string source)
+        {
+            var lines = source.Split('\n').ToList();
+
+            Stack stack = new Stack();
+
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("p("))
+                {
+                    // p(x) = 1
+                    var s = line.Split('=');
+                    int pointer = Convert.ToInt32(s.Last());
+
+                    if (stack.Count - 1 < pointer)
+                    {
+
+                    }
+                }
+                else
+                {
+                    // SYNTAX ERROR
+                }
+            }
+
+            return stack;
+        }
+    }
+}
